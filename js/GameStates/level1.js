91e641df07b7bc757a26ea85468d8b13a2de141a
GojoSatoru1738 <ajmiller@email.neit.edu>
2026-03-02 10:07:37 -0500

    yuh

--- js/GameStates/level1.js
+++ js/GameStates/level1.js
@@ -161,7 +161,7 @@ gameStates[`level1`] = function()
 			shotTimer = shotDelay
 			//console.log(`Boom`)
 
-			bullets[currentBullet].vx = 5*wiz.dir;
+			bullets[currentBullet].vx = 10*wiz.dir;
 			bullets[currentBullet].world = level;
 			bullets[currentBullet].x = wiz.x-level.x + (wiz.dir * 96) ;
 			bullets[currentBullet].y = wiz.y + 20;
